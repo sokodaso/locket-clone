566bc9e12cab10d6d930c044db1fd1aa5d321d2a
sokodaso <sokodaso@purdue.edu>
2026-05-23 18:06:01 -0400

    New Post wired to backend

--- client/my-app/src/posts/pages/NewPost.tsx
+++ client/my-app/src/posts/pages/NewPost.tsx
@@ -1,21 +1,46 @@
 import {Box, Typography, Button, TextField,Card} from "@mui/material";
-import { useFormStatus } from "react-dom";
+import {useState} from 'react';
 
 function NewPost() {
-   const {pending} = useFormStatus();
+   const [isSubmitting, setIsSubmitting] = useState(false);
+   const [title, setTitle] = useState('');
+   const [content, setContent] = useState('');
+
+    const handleCreatePost = async (e: any) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+        try{
+            await fetch(`http://localhost:3000/api/posts`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title: title,
+                    content: content, 
+                    authorId: '2'
+                })
+            });
+        } catch (error) {
+             console.error('Error submitting post:', error);
+        }finally {
+            setIsSubmitting(false);
+        }
+    };
 
   return (
     <Box>
         <Typography variant="h5">New Post</Typography>
         <Card sx={{padding: 2, marginTop: 2}}>
-            <form method="post">
-                <TextField label="Title" variant="outlined" />
-                <TextField label="Content" variant="outlined" multiline rows={4} />
-                <Button variant="contained" color="primary" disabled={pending}>
-                    Create Post
+            <form method="post" onSubmit={handleCreatePost}>
+                <TextField label="Title" variant="outlined" value={title} onChange={(e) => setTitle(e.target.value)} />
+                <TextField label="Content" variant="outlined" multiline rows={4} value={content} onChange={(e) => setContent(e.target.value)} />
+                <Button  type="submit" variant="contained" color="primary">
+                    {isSubmitting ? 'Submitting...' : 'Create Post'}
                 </Button>
             </form>
         </Card>
+
     </Box>
   );
 }
