535dd0fb438646ecd0a6a96a28e77c9fcf607f4d
sokodaso <sokodaso@purdue.edu>
2026-06-15 16:02:01 -0400

    Signing In and LogOut

--- client/my-app/src/shared/components/AppBar.tsx
+++ client/my-app/src/shared/components/AppBar.tsx
@@ -1,10 +1,13 @@
 import {Box, Typography,Button} from "@mui/material";
 import {Outlet, useNavigate, useParams, useLocation} from "react-router";
+import { useContext } from "react";
+import AuthContext from "../../context/Auth-Context";
 
 function AppBar() {
     const navigate = useNavigate();
     const location = useLocation();
     const {userId} = useParams();
+    const auth = useContext(AuthContext);
 
     const isPostsPage = location.pathname.includes('/posts');
     
@@ -13,15 +16,20 @@ function AppBar() {
             <Box className="mobile-app-container" sx={{ maxWidth: { xs: '100%', sm: '600px', md: '5000px' }, margin: '0 auto', display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
                 <header style={{display: 'flex',justifyContent: 'space-between', padding: '1.5rem', width: '100%', margin: '0 auto', boxShadow: '0 2px 4px rgba(0,0,0,0.1)', boxSizing: 'border-box'}}> 
                     <Typography variant="h6">LocketStyle</Typography>
-                    {isPostsPage ? (
-                        <Button variant="contained" color="primary" onClick={() => navigate(`/users/${userId}`)}>
-                        Back to Profile
+                    <Box sx={{ display: 'flex', gap: '8px' }}>
+                        {isPostsPage ? (
+                            <Button variant="contained" color="primary" onClick={() => navigate(`/users/${userId}`)}>
+                            Back to Profile
+                            </Button>
+                        ) : ( 
+                            <Button variant="contained" color="primary" onClick={() => navigate(`/posts/new`)}>
+                                Add Posts
+                            </Button>
+                        )}
+                        <Button variant="contained" color="primary" onClick={() => auth.logout()}>
+                            Logout
                         </Button>
-                    ) : ( 
-                        <Button variant="contained" color="primary" onClick={() => navigate(`/posts/new`)}>
-                            Add Posts
-                        </Button>
-                    )}
+                    </Box>
                 </header>
                 <main style={{padding: '16px', marginTop: '16px'}}>
                     <Outlet />
