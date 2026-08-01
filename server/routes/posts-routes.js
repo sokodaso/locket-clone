9dc620dbdfefb763e140200b37b9d173f05bbe14
sokodaso <sokodaso@purdue.edu>
2026-07-31 20:32:11 -0700

    chore: minor edit

--- server/routes/posts-routes.js
+++ server/routes/posts-routes.js
@@ -10,16 +10,7 @@ router.use(checkAuth);
 router.get('/:uid', getPostsByUserId);
 
 //POST api/posts
-router.post('/',(req, res, next)=>{
-    
-    fileUpload.single('image')(req, res, (err)=>{
-        if(err){
-            console.error('Error uploading file:', err);
-            return res.status(500).json({ message: 'Error uploading file' });
-        }
-        next();
-    });
-}, createPost);
+router.post('/', fileUpload.single('image'), createPost);
 
 //UPDATE api/posts/:pid
 router.patch('/:pid', updatePost);
